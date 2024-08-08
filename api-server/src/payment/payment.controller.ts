6e255eec5031ae4592a202e7c1f5c9926d795936
jds <smaa11271@gmail.com>
2024-08-08 16:28:58 +0900

    payment api 엔드포인트 권한처리

--- api-server/src/payment/payment.controller.ts
+++ api-server/src/payment/payment.controller.ts
@@ -6,15 +6,15 @@ import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 @UseGuards(JwtAuthGuard)
 @Controller('payment')
 export class PaymentController {
-  constructor(private readonly paymentService: PaymentService) {}
+	constructor(private readonly paymentService: PaymentService) { }
 
-  @Post()
-  create(@Body() createBasketDtoList: CreateBasketDto[]) {
-    return this.paymentService.create(createBasketDtoList);
-  }
+	@Post()
+	create(@Body() createBasketDtoList: CreateBasketDto[]) {
+		return this.paymentService.create(createBasketDtoList);
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.paymentService.findOne(+id);
-  }
+	@Get(':id')
+	findOne(@Param('id') id: string) {
+		return this.paymentService.findOne(+id);
+	}
 }
